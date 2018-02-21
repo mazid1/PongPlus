2af4f7fbb02ec60abbbd2a232f7e670b264fe1f7
mazid1 <mazidmailbox@gmail.com>
2018-02-21 22:58:01 +0600

    Collision between 2 balls behaviour changed

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,11 +10,15 @@ public class BallController : MonoBehaviour {
     private int horizontalForce = 30;
     //private float time = 0.0f;
     private float speedMultiplier = 1.0005f;
+    private float maxVelocityY = 7.0f;
+    private float maxVelocityX = 1.5f;
+    private ContactPoint2D[] contactPoints;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         mRigidbody2D = GetComponent<Rigidbody2D>();
         velocity = new Vector2();
+        contactPoints = new ContactPoint2D[10];
         Invoke("GoBall", 2);
 	}
 
@@ -45,8 +49,38 @@ public class BallController : MonoBehaviour {
 
     private void OnCollisionEnter2D (Collision2D collision) {
         if (collision.collider.CompareTag("Player")) {
-            velocity.x = mRigidbody2D.velocity.x;
-            velocity.y = (mRigidbody2D.velocity.y / 1.2f) + (collision.collider.attachedRigidbody.velocity.y / 3.0f);
+            collision.GetContacts (contactPoints);
+
+            if (mRigidbody2D.velocity.x > 0) {
+                velocity.x =
+                    mRigidbody2D.velocity.x
+                    + (Mathf.Abs ((collision.transform.InverseTransformPoint (contactPoints[0].point)).y)
+                    * maxVelocityX);
+            } else {
+                velocity.x = 
+                    mRigidbody2D.velocity.x 
+                    - (Mathf.Abs ((collision.transform.InverseTransformPoint (contactPoints[0].point)).y) 
+                    * maxVelocityX);
+            }
+
+            if (velocity.x < 5.0f && velocity.x > -5.0f) {
+                if (velocity.x > 0)
+                    velocity.x = 5.0f;
+                else
+                    velocity.x = -5.0f;
+            }
+
+            velocity.y = (collision.transform.InverseTransformPoint (contactPoints[0].point)).y * maxVelocityY;
+            mRigidbody2D.velocity = velocity;
+        }
+        else if (collision.collider.CompareTag("Ball")) {
+            velocity = mRigidbody2D.velocity;
+            if (velocity.x < 5.0f && velocity.x > -5.0f) {
+                if (velocity.x > 0)
+                    velocity.x = 5.0f;
+                else
+                    velocity.x = -5.0f;
+            }
             mRigidbody2D.velocity = velocity;
         }
     }
